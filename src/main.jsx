c731eaf8fac413f160fc62e584658f63384c653e
Deni Sugiarto <sugiarto.deni@gmail.com>
2023-10-12 17:57:10 +0700

    add mui and stepper

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import { ThemeProvider, createTheme } from '@mui/material';
+
+const theme = createTheme({
+	components: {
+		// Name of the component
+		MuiButtonBase: {
+			defaultProps: {
+				// The props to change the default for.
+				disableRipple: true, // No more ripple, on the whole application 💣!
+			},
+		},
+	},
+});
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+	<React.StrictMode>
+		<ThemeProvider theme={theme}>
+			<App />
+		</ThemeProvider>
+	</React.StrictMode>
+);
